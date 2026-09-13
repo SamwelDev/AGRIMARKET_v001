51a84050f34f4d45df559b19c5ecf2f46cb83343
Samwel David Mwamakula <samwelmwamakula@gmail.com>
2026-09-13 19:00:28 +0300

    [BUILD] added of use cases layer touched i.e.Application layer.

---
+++ AGRIMARKET.RESOURCES/RESOURCES.DTOS/DTO.MKT/MarketDto.cs
@@ -0,0 +1,38 @@
+﻿using AGRIMARKET.RESOURCES.RESOURCE.ENUMS;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AGRIMARKET.RESOURCES.RESOURCES.DTOS.DTO.MKT;
+
+public  class MarketDto
+{
+}
+public class PriceDto
+{
+    public long Id { get; set; }
+    public decimal Price { get; set; }
+    public CurrenyEnum Curreny { get; set; } = CurrenyEnum.TZS;
+    public PriceType PriceType { get; set; } = PriceType.RETAIL;
+    public DateTimeOffset RecordedAt { get; set; } = DateTimeOffset.Now;
+    public long SourceId { get; set; }
+    public long? CommodityId { get; set; }
+    public long? MarketId { get; set; }
+
+}
+public class SourceDto
+{
+    public long Id { get; set; }
+    public string? Name { get; set; }
+    //Collection
+    public ICollection<PriceDto> Prices { get; set; } = new HashSet<PriceDto>();
+}
+public class CommodityDto
+{
+    public long Id { get; set; }
+    public string? Name { get; set; }
+    public UnitEnum Unit { get; set; } = UnitEnum.KG;
+
+    public ICollection<PriceDto> Prices { get; set; } = new HashSet<PriceDto>();
+
+}
---
+++ AGRIMARKET.RESOURCES/RESOURCES.DTOS/DTO.STR/RegionDto.cs
@@ -0,0 +1,22 @@
+﻿using AGRIMARKET.RESOURCES.RESOURCES.DTOS.DTO.MKT;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AGRIMARKET.RESOURCES.RESOURCES.DTOS.DTO.STR;
+
+public  class RegionDto
+{
+    public long Id { get; set; }
+    public string? Name { get; set; }
+
+    //Collection
+    public ICollection<DistictDto> Districts { get; set; } = new HashSet<DistictDto>();
+}
+public class DistictDto
+{
+    public long Id { get; set; }
+    public string? Name { get; set; }
+    //Collection
+    public ICollection<MarketDto> Markets { get; set; } = new HashSet<MarketDto>();
+}
