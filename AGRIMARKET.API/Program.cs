151a0ec34d0b19f1405ddec58990aaa8f888d8a9
Samwel David Mwamakula <samwelmwamakula@gmail.com>
2026-09-13 17:46:12 +0300

    [BUILD] added configuration of scalar api in api layer .

--- AGRIMARKET.API/Program.cs
+++ AGRIMARKET.API/Program.cs
@@ -1,8 +1,22 @@
+using Microsoft.AspNetCore.RateLimiting;
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddRateLimiter(opt =>
+{
+    opt.AddFixedWindowLimiter("Windows-Policy", limiter =>
+    {
+        limiter.Window = TimeSpan.FromMinutes(1);
+        limiter.PermitLimit = 100;
+        limiter.QueueLimit = 0;
+        limiter.QueueProcessingOrder = System.Threading.RateLimiting.QueueProcessingOrder.OldestFirst;
+    });
+    opt.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
+});
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
@@ -12,6 +26,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
